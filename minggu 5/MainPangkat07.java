c0d19e09e828bdc94c2cf196988240d50936330f
Aureylio <aureyliorangga2@gmail.com>
2026-03-10 10:20:44 +0700

    Pangkat07

--- minggu 5/MainPangkat07.java
+++ minggu 5/MainPangkat07.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 public class MainPangkat07 {
-   
+    
     public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Masukkan jumlah elemen: ");
         int elemen = input.nextInt();
 
-        Pangkat07[] png = new Pangkat07[elemen];
-        for (int i=0; i<elemen; i++){
-            System.out.print("Masukkan nilai basis elemen ke-"+(i+1)+": ");
+        Pangkat07[] pangkat = new Pangkat07[elemen];
+        for(int i= 0; i<elemen;i++){
+            System.out.print("Masukkan nilai basis elemen ke-" + (i+1) + ": ");
             int basis = input.nextInt();
-            System.out.print("Masukkan nilai pangkat elemen ke-"+(i+1)+": ");
-            int pangkat = input.nextInt();
-            png[i] = new Pangkat07(basis, pangkat);
+            System.out.print("Masukkan nilai pangkat elemen ke-" + (i+1) + ": ");
+            int pangkat1 = input.nextInt();
+            pangkat[i] = new Pangkat07();
+            pangkat[i].pangkat(basis, pangkat1);
         }
-        System.out.println("HASIL PANGKAT BRUTEFORCE");
-        for (Pangkat07 p : png) {
-             System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatBF(p.nilai, p.pangkat));
+
+        System.out.println("\nHASIL PANGKAT BRUTEFORCE: ");
+        for(Pangkat07 p : pangkat){
+            System.out.println(p.nilai+"^"+p.pangkat+" : " + p.pangkatBF(p.nilai, p.pangkat));
         }
-        
-        System.out.println("HASIL PANGKAT DIVIDE AND CONQUER");
-        for (Pangkat07 p : png) {
-            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatDC(p.nilai, p.pangkat)); 
+
+        System.out.println("\nHASIL PANGKAT DIVIDE AND CONQUER: ");
+        for(Pangkat07 p : pangkat){
+            System.out.println(p.nilai+"^"+p.pangkat+" : " + p.pangkatDC(p.nilai, p.pangkat));
         }
     }
 }
--- minggu 5/Pangkat07.java
+++ minggu 5/Pangkat07.java
@@ -1,28 +1,29 @@
 public class Pangkat07 {
-    
     int nilai, pangkat;
-    Pangkat07(int n, int p){
+
+    int pangkat(int n, int p){
         nilai = n;
         pangkat = p;
-    }
+        return nilai;
+    } 
 
-    int pangkatBF(int a, int n){
-        int hasil = 1; 
-        for(int i=0; i<n; i++){
-            hasil = hasil*a;
+    int pangkatBF(int a, int b) {
+        int hasil = 1;
+        for(int i=1; i<=b; i++) {
+            hasil = hasil * a;
         }
         return hasil;
     }
 
-    int pangkatDC(int a, int n){
-        if(n==1){
+    int pangkatDC(int a, int n) {
+        if (n==1){
             return a;
-        }else{
+        } else {
             if(n%2==1){
-                return (pangkatDC(a, n/2)*pangkatDC(a, n/2)*a);
-            }else{
-                return (pangkatDC(a, n/2)*pangkatDC(a, n/2));
-            }
+                return (pangkatDC(a, n/2) * pangkatDC(a, n/2) * a);
+            } else {
+                return (pangkatDC(a, n/2) * pangkatDC(a, n/2));
             }
         }
     }
+}
